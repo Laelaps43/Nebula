11b0a05f740f60628bf873eca19dfb243d75a7af
laelaps <laelaps43@gmail.com>
2023-09-10 23:14:19 +0800

    前端代码

---
+++ web/src/router/router.config.ts
@@ -0,0 +1,140 @@
+import BasicLayout from '/@/layouts/BasicLayout/index.vue';
+import BlankLayout from '/@/layouts/BlankLayout.vue';
+import type { RouteRecordRaw } from 'vue-router';
+
+export const accessRoutes: RouteRecordRaw[] = [
+  {
+    path: '/app',
+    name: 'app',
+    component: BasicLayout,
+    redirect: '/app/home',
+    meta: { title: '管理平台' },
+    children: [
+      {
+        path: '/app/home',
+        component: () => import('/@/views/home/index.vue'),
+        name: 'home',
+        meta: {
+          title: '数据',
+          icon: 'liulanqi',
+          auth: ['home'],
+        },
+      },
+      {
+        path: '/app/website',
+        name: 'website',
+        component: () => import('/@/views/website/index.vue'),
+        meta: {
+          title: '设备列表',
+          keepAlive: true,
+          icon: 'jiedianguanli',
+          auth: ['website'],
+        },
+      },
+      {
+        path: '/app/table-demo',
+        name: 'table-demo',
+        component: () => import('/@/views/table-demo/index.vue'),
+        meta: {
+          title: '远端录像',
+          keepAlive: true,
+          icon: 'rili',
+        },
+      },
+      {
+        path: '/app/record',
+        name: 'table-recor',
+        component: () => import('/@/views/table-demo/index.vue'),
+        meta: {
+          title: '用户管理',
+          keepAlive: true,
+          icon: 'rili',
+        },
+      },
+      {
+        path: '/app/others',
+        name: 'others',
+        component: BlankLayout,
+        redirect: '/app/others/about',
+        meta: {
+          title: '系统设置',
+          icon: 'shurumimadenglu',
+          auth: ['others'],
+        },
+        children: [
+          {
+            path: '/app/others/about',
+            name: 'about',
+            component: () => import('/@/views/others/about/index.vue'),
+            meta: { title: '信令服务', keepAlive: true, hiddenWrap: true },
+          },
+          {
+            path: '/app/others/antdv',
+            name: 'antdv',
+            component: () => import('/@/views/others/antdv/index.vue'),
+            meta: { title: '媒体服务', keepAlive: true, breadcrumb: true },
+          },
+          {
+            path: '/app/others/login',
+            name: 'login',
+            component: () => import('/@/views/others/antdv/index.vue'),
+            meta: { title: '系统日志', keepAlive: true, breadcrumb: true },
+          },
+        ],
+      },
+      {
+        path: '/sys/account',
+        name: 'account',
+        component: () => import('/@/views/account/index.vue'),
+        meta: { title: '用户管理', keepAlive: true, breadcrumb: true },
+      },
+    ],
+  },
+];
+
+const constantRoutes: RouteRecordRaw[] = [
+  {
+    path: '/login',
+    component: () => import('/@/views/login/index.vue'),
+    name: 'login',
+    meta: { title: '登录' },
+  },
+  {
+    path: '/',
+    name: 'Root',
+    redirect: '/app',
+    meta: {
+      title: 'Root',
+    },
+  },
+  // ...accessRoutes,
+];
+
+export const publicRoutes = [
+  {
+    path: '/redirect',
+    component: BlankLayout,
+    children: [
+      {
+        path: '/redirect/:path(.*)',
+        component: () => import('/@/views/redirect/index'),
+      },
+    ],
+  },
+  {
+    path: '/:pathMatch(.*)',
+    redirect: '/404',
+  },
+  {
+    path: '/404',
+    component: () => import('/@/views/404.vue'),
+  },
+];
+
+// /**
+//  * 基础路由
+//  * @type { *[] }
+//  */
+// export const constantRouterMap = [];
+
+export default constantRoutes;
---
+++ web/src/views/home/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="abc">
+    <!-- search -->
+    <!-- chart -->
+    <a-row :gutter="24">
+      <a-col :span="12">
+        <a-card :bordered="false">
+          <DataOverview />
+        </a-card>
+      </a-col>
+      <a-col :span="12">
+        <a-card :bordered="false">
+          <!-- <TradingHistory :loading="loading" /> -->
+        </a-card>
+      </a-col>
+    </a-row>
+    <a-divider class="line" />
+    <a-row :gutter="24">
+      <a-col :span="12">
+        <a-card :bordered="false">
+          <TradingHistory :loading="loading" />
+        </a-card>
+      </a-col>
+      <a-col :span="12">
+        <a-card :bordered="false">
+          <TradingHistory :loading="loading" />
+        </a-card>
+      </a-col>
+    </a-row>
+    <!-- <Table :url="fetchApi.list" :columns="columns" :hiddenFilter="true" :scroll="{ x: 1200 }" /> -->
+  </div>
+</template>
+<script setup lang="ts">
+  import DataOverview from './components/DataOverview.vue';
+  import TradingHistory from './components/TradingHistory.vue';
+  // import { columns } from './constant';
+  // import fetchApi from '/@/api/home';
+  import { useHomeStore } from '/@/store/modules/home';
+
+  const store = useHomeStore();
+  const loading = ref(false);
+
+  onMounted(async () => {
+    loading.value = true;
+    await store.fetchInfo();
+    loading.value = false;
+  });
+
+  onUnmounted(() => {
+    store.resetState();
+  });
+</script>
